a303873b0694d3e25f8af6c550de4ca044ebbecf
Fachrul_Faathirullah <lenpuchongloi93@gmail.com>
2021-05-23 18:01:31 +0700

    dom manipulation 2

--- README.md
+++ README.md
@@ -1,5 +1,3 @@
-# javascript-dan-DOM
-
 dom : antarmuka pemograman berbasis object representasi dokumen web
 dom : buat seluruh komponen dari halaman we dapat diakses dan dimanipulasi
 komponen : element html, atribut, text , dll
@@ -9,9 +7,29 @@ root node: sumber dri node lain
 parent node : 1 tingkat diatas node yg lain cth : body parent dri h1, p, div
 child node : berada 1 tingkat dibawah node lain
 
-Dom selection Method : 
+Dom selection Method :
 getElementById() : element
 getElementsByTagName() : htmlCollection
 getElementsByClassName() : htmlCollection
 querySelector() : elements
 querySelectorAll() : nodeList
+
+pilih yang mana :
+target :
+
+paling cepat :
+
+Dom selection Method :
+getElementById() : element
+getElementsByTagName() : htmlCollection
+
+tidak mengubah structure html / menambah id / class :
+querySelector() : elements
+querySelectorAll() : nodeList
+
+manipulasi atribut :
+getAttribute
+setAttribute
+
+classList:
+add, remove , toogle(jika ada class dihilangkan), item (mengetahui class tertentu), contains (punya class tertentu), replace (ganti class)
--- script.js
+++ script.js
@@ -1,37 +1,45 @@
-//mengembalikan element
-const judul = document.getElementById("judul");
-// judul.style.color = "red";
-// judul.style.backgroundColor = "gray";
-judul.innerHTML = "<em>Fachrul Faathirullah</em>";
+// //mengembalikan element
+// const judul = document.getElementById("judul");
+// // judul.style.color = "red";
+// // judul.style.backgroundColor = "gray";
+// judul.innerHTML = "<em>Fachrul Faathirullah</em>";
 
-// //document.getElementsByTagName
-// const p = document.getElementsByTagName("p");
-// for (let i = 0; i < p.length; i++) {
-//   p[i].style.backgroundColor = "red";
-// }
+// // //document.getElementsByTagName
+// // const p = document.getElementsByTagName("p");
+// // for (let i = 0; i < p.length; i++) {
+// //   p[i].style.backgroundColor = "red";
+// // }
 
-// const h1 = document.getElementsByTagName("h1")[0];
-// h1.style.fontSize = "50px";
+// // const h1 = document.getElementsByTagName("h1")[0];
+// // h1.style.fontSize = "50px";
 
-//document.querySelector()
-// const p4 = document.querySelector("#b p");
+// //document.querySelector()
+// // const p4 = document.querySelector("#b p");
+// // p4.style.color = "green";
+// // p4.style.fontSize = "24px";
+
+// const li2 = document.querySelector("section#b ul li:nth-child(2)");
+// li2.style.backgroundColor = "orange";
+
+// const p = document.querySelectorAll("p");
+// p[2].style.backgroundColor = "yellow";
+
+// const sectionB = document.querySelector("section#b");
+// const p4 = sectionB.getElementsByTagName("p")[0];
 // p4.style.color = "green";
 // p4.style.fontSize = "24px";
 
-const li2 = document.querySelector("section#b ul li:nth-child(2)");
-li2.style.backgroundColor = "orange";
+// const sectionA = document.querySelector("section#a");
 
-const p = document.querySelectorAll("p");
-p[2].style.backgroundColor = "yellow";
+// sectionA.innerHTML = " hellow world";
 
-const sectionB = document.querySelector("section#b");
-const p4 = sectionB.getElementsByTagName("p")[0];
-p4.style.color = "green";
-p4.style.fontSize = "24px";
+// const judul1 = document.getElementsByTagName("h1")[0];
+// judul1.setAttribute("name", "fachrul");
 
-const sectionA = document.querySelector("section#a");
+const pBaru = document.createElement("p");
+const pBaru2 = document.createTextNode("paragraf baru");
 
-sectionA.innerHTML = " hellow world";
+pBaru.appendChild(pBaru2);
 
-const judul1 = document.getElementsByTagName("h1")[0];
-judul1.setAttribute("name", "fachrul");
+const sectionA = document.getElementById("a");
+sectionA.appendChild(pBaru);
